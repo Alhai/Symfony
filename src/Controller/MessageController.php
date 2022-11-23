2e0526f854b3595432604b130191ca8c9b31c6e8
Aly_H <77671706+Alhai@users.noreply.github.com>
2022-11-23 13:55:34 +0100

    create in bdd

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Message;
+use App\Form\MessageType;
 use App\Repository\MessageRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,6 +23,27 @@ class MessageController extends AbstractController
         return $this->render('pages/message/index.html.twig',[
             'messages' => $messages
             // 'controller_name' => 'MessageController',
+        ]);
+    }
+
+    #[Route('/message/nouveau', 'message.new', methods:['GET', 'POST'])]
+    public function new(
+        Request $request,
+        EntityManagerInterface $manager
+    ) : Response 
+    {
+        $message = new Message();
+        $form = $this->createForm(MessageType::class, $message);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $message = $form->getData();
+            $manager->persist($message);
+            $manager->flush();
+        }
+
+        return $this->render('pages/message/new.html.twig', [
+            'form' => $form->createView()
+
         ]);
     }
 }
--- src/Form/MessageType.php
+++ src/Form/MessageType.php
@@ -4,16 +4,39 @@ namespace App\Form;
 
 use App\Entity\Message;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class MessageType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('message')
-        ;
+            ->add('message', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlength' => '1',
+                    'maxlength' => '255'
+                ],
+                'label' => 'Message',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+                'constraints' => [
+                    new Assert\Length(['min' => 1, 'max' => 255]),
+                    new Assert\NotBlank()
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-primary mt-4'
+                ],
+               
+                'label' => 'Quacker'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
