2a1804b6c9c13425855bf6f91a1eaee634e097dd
Aly_H <77671706+Alhai@users.noreply.github.com>
2022-11-23 11:07:25 +0100

    get all

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -1,5 +1,8 @@
 <?php
 
+namespace App\Controller;
+
+use App\Repository\MessageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -7,9 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class MessageController extends AbstractController
 {
     #[Route('/message', name: 'message')]
-    public function index(): Response
+    public function index(
+        MessageRepository $repository
+    ): Response
     {
+        $messages = $repository->findAll();
         return $this->render('pages/message/index.html.twig',[
+            'messages' => $messages
             // 'controller_name' => 'MessageController',
         ]);
     }
