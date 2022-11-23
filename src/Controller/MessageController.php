b19af390d4f70fe9599d2a007fa9750f7e2dfb62
Aly_H <77671706+Alhai@users.noreply.github.com>
2022-11-23 10:10:39 +0100

    create MessageController

---
+++ src/Controller/MessageController.php
@@ -0,0 +1,16 @@
+<?php
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MessageController extends AbstractController
+{
+    #[Route('/message', name: 'message')]
+    public function index(): Response
+    {
+        return $this->render('message/index.html.twig',[
+            'controller_name' => 'MessageController',
+        ]);
+    }
+}
